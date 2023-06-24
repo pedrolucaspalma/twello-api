32f650a8310c61ee229e1acdecba47fa6a16c35a
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-24 11:10:13 -0300

    add factory method

--- src/factories/ServicesFactory.ts
+++ src/factories/ServicesFactory.ts
@@ -1,9 +1,14 @@
 import { BoardDao } from "../daos/BoardDao";
 import { UserDao } from "../daos/UserDao";
+import { BoardService } from "../services/BoardService";
 import { UserService } from "../services/UserService";
 
 export class ServicesFactory {
 	static makeUserService() {
 		return new UserService(new UserDao(), new BoardDao());
+	}
+
+	static makeBoardService() {
+		return new BoardService(new BoardDao());
 	}
 }
