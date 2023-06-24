a33bfe3e709944a5130bd323119858c7bc0cf922
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-24 10:15:56 -0300

    add board and sharedBoards table

--- src/database.ts
+++ src/database.ts
@@ -1,9 +1,10 @@
-import { Board } from "./types/BoardTypes";
+import { Board, UsersSharedBoardAssociation } from "./types/BoardTypes";
 import { User } from "./types/UserTypes";
 
 export const boardsTable: Board[] = [
 	{
 		uuid: "2713c147-85ee-46a0-b23b-b9cce1a8b8e8",
+		creatorUserUuid: "74aa8bd0-ff7c-449b-8c94-a0513178ab94",
 		title: "Trabalho",
 		backgroundColor: "#FF5733",
 		textColor: "#000000",
@@ -11,17 +12,19 @@ export const boardsTable: Board[] = [
 	},
 	{
 		uuid: "2713c147-85ee-46a0-b23b-b9cce1a8b8e8",
+		creatorUserUuid: "69cf3341-b40d-45d2-b602-313d83b5df5e",
 		title: "Faculdade",
 		backgroundColor: "#2C8BCD",
 		textColor: "#000000",
 		createdAt: 1687573903886,
 	},
+];
+
+export const usersSharedBoardsTable: UsersSharedBoardAssociation[] = [
 	{
-		uuid: "4a7e939a-f3d7-4b38-9491-53077c26e88f",
-		title: "Casa",
-		backgroundColor: "#1D4C20",
-		textColor: "#000000",
-		createdAt: 1687573903886,
+		boardUuid: "2713c147-85ee-46a0-b23b-b9cce1a8b8e8",
+		userUuid: "69cf3341-b40d-45d2-b602-313d83b5df5e",
+		canEdit: true,
 	},
 ];
 
