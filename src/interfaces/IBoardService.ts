a97b59b5e09646485a3c033b785dfedc1f7fe12d
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-24 14:45:26 -0300

    add board update methods

--- src/interfaces/IBoardService.ts
+++ src/interfaces/IBoardService.ts
@@ -1,5 +1,14 @@
-import { BoardCreationPayload } from "../types/BoardTypes";
+import { BoardCreationPayload, BoardUpdatePayload } from "../types/BoardTypes";
 
 export interface IBoardService {
 	createBoard(payload: BoardCreationPayload): Promise<void>;
+	updateBoard(
+		boardUuid: string,
+		requestingUserUuid: string,
+		data: BoardUpdatePayload
+	): Promise<void>;
+	isUserAllowedToEditBoard(
+		userUuid: string,
+		boardUuid: string
+	): Promise<boolean>;
 }
--- src/services/BoardService.ts
+++ src/services/BoardService.ts
@@ -1,11 +1,42 @@
 import { IBoardDao } from "../interfaces/IBoardDao";
 import { IBoardService } from "../interfaces/IBoardService";
-import { BoardCreationPayload } from "../types/BoardTypes";
+import { BoardCreationPayload, BoardUpdatePayload } from "../types/BoardTypes";
+import { StatusError } from "../utils/StatusErrors";
 
 export class BoardService implements IBoardService {
 	constructor(private readonly boardDao: IBoardDao) {}
 
 	async createBoard(payload: BoardCreationPayload): Promise<void> {
 		await this.boardDao.createBoard(payload);
+	}
+
+	async updateBoard(
+		boardUuid: string,
+		requestingUserUuid: string,
+		data: BoardUpdatePayload
+	): Promise<void> {
+		const board = await this.boardDao.getBoard(boardUuid);
+		if (!board) throw new StatusError(404, "Board not found");
+
+		const isAllowed = await this.isUserAllowedToEditBoard(
+			requestingUserUuid,
+			boardUuid
+		);
+		if (!isAllowed)
+			throw new StatusError(401, "User unauthorized to perform this action");
+
+		await this.boardDao.updateBoard(boardUuid, data);
+	}
+
+	async isUserAllowedToEditBoard(
+		userUuid: string,
+		boardUuid: string
+	): Promise<boolean> {
+		const association = await this.boardDao.getUserAssociationWithBoard(
+			boardUuid,
+			userUuid
+		);
+		if (!association) return false;
+		return association.canEdit;
 	}
 }
