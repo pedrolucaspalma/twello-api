cff55a4b4d526fe73410e002e2ce365099ecae40
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-24 09:15:49 -0300

    Move crypt functions to utils class

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -1,11 +1,7 @@
-import bcrypt from "bcryptjs";
-import jsonwebtoken from "jsonwebtoken";
-
 import { IUserDao } from "../interfaces/IUserDao";
 import { IUserService } from "../interfaces/IUserService";
 import { CreateUserPayload, SignInPayload } from "../types/UserTypes";
-
-const { SECRET } = process.env;
+import { CryptUtil } from "../utils/CryptUtil";
 
 export class UserService implements IUserService {
 	constructor(private readonly userDao: IUserDao) {}
@@ -15,7 +11,7 @@ export class UserService implements IUserService {
 		);
 		if (!emailIsAvailable) throw new Error("email already in use");
 
-		const encryptedPassword = await this.hashPassword(userData.password);
+		const encryptedPassword = await CryptUtil.hashPassword(userData.password);
 		const formattedData = { ...userData, password: encryptedPassword };
 		await this.userDao.create(formattedData);
 	}
@@ -24,40 +20,18 @@ export class UserService implements IUserService {
 		const user = await this.userDao.findByEmail(signInData.email);
 		if (!user) throw new Error("Invalid credentials");
 
-		const passwordMatches = await this.comparePasswords(
+		const passwordMatches = await CryptUtil.comparePasswords(
 			signInData.password,
 			user.password
 		);
 		if (!passwordMatches) throw new Error("Invalid credentials");
 
-		const token = this.generateJWT({
+		const token = CryptUtil.generateJWT({
 			uuid: user.uuid,
 			email: user.email,
 			name: user.name,
 		});
 
-		return token;
-	}
-
-	private async hashPassword(password: string): Promise<string> {
-		const salt = await bcrypt.genSalt(10);
-		const hashedPassword = await bcrypt.hash(password, salt);
-		return hashedPassword;
-	}
-
-	private async comparePasswords(
-		payloadPassword: string,
-		userPassword: string
-	): Promise<boolean> {
-		return bcrypt.compare(payloadPassword, userPassword);
-	}
-
-	private generateJWT(userData: {
-		uuid: string;
-		name: string;
-		email: string;
-	}): string {
-		const token = jsonwebtoken.sign(userData, SECRET as string);
 		return token;
 	}
 }
---
+++ src/utils/CryptUtil.ts
@@ -0,0 +1,26 @@
+import bcrypt from "bcryptjs";
+import jsonwebtoken from "jsonwebtoken";
+const { SECRET } = process.env;
+
+export class CryptUtil {
+	static async hashPassword(password: string): Promise<string> {
+		const salt = await bcrypt.genSalt(10);
+		const hashedPassword = await bcrypt.hash(password, salt);
+		return hashedPassword;
+	}
+
+	static async comparePasswords(
+		payloadPassword: string,
+		userPassword: string
+	): Promise<boolean> {
+		return bcrypt.compare(payloadPassword, userPassword);
+	}
+	static generateJWT(userData: {
+		uuid: string;
+		name: string;
+		email: string;
+	}): string {
+		const token = jsonwebtoken.sign(userData, SECRET as string);
+		return token;
+	}
+}
