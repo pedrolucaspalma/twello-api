3019ff2fef306e45bfd480bf896d823a21b6e831
Pedro Palma <pedrolucaspalma@gmail.com>
2023-06-24 10:56:59 -0300

    Remove typeorm configs from project

--- src/index.ts
+++ src/index.ts
@@ -6,46 +6,36 @@ import bodyParser from "body-parser";
 
 import router from "./routes";
 
-import { AppDataSource } from "./data-source";
 import { StatusError, StatusErrorType } from "./utils/StatusErrors";
 
-AppDataSource.initialize()
-	.then(async () => {
-		dotenv.config();
-		const app = express();
-		const port = process.env.PORT;
+dotenv.config();
+const app = express();
+const port = process.env.PORT;
 
-		// Adding body-parser to be able to access req.body
-		app.use(bodyParser.urlencoded({ extended: false }));
-		app.use(bodyParser.json());
+// Adding body-parser to be able to access req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-		// Adding app routes
-		app.use(router);
+// Adding app routes
+app.use(router);
 
-		// Adding middlewares to be called after every route.
-		// 404 error middleware
-		app.use((req: Request, res: Response, next: NextFunction) =>
-			next(new StatusError(404, "Route not found"))
-		);
-		// Global Error handler middleware
-		app.use(
-			(
-				err: StatusErrorType,
-				req: Request,
-				res: Response,
-				next: NextFunction
-			) => {
-				console.log(`[error]: ${err.message}`);
-				if (err.message && err.status)
-					return res.status(err.status).send({ message: err.message });
+// Adding middlewares to be called after every route.
+// 404 error middleware
+app.use((req: Request, res: Response, next: NextFunction) =>
+	next(new StatusError(404, "Route not found"))
+);
+// Global Error handler middleware
+app.use(
+	(err: StatusErrorType, req: Request, res: Response, next: NextFunction) => {
+		console.log(`[error]: ${err.message}`);
+		if (err.message && err.status)
+			return res.status(err.status).send({ message: err.message });
 
-				return res.status(500).send({ message: "Internal server Error" });
-			}
-		);
+		return res.status(500).send({ message: "Internal server Error" });
+	}
+);
 
-		// Initializing server
-		app.listen(port, () => {
-			console.log(`[server]: Server is running at http://localhost:${port}`);
-		});
-	})
-	.catch((error) => console.log(error));
+// Initializing server
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
