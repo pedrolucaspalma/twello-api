e6a249a37578ca578fdf38c88fb5e16fa69cfdcf
pedrolucaspalma <pedrolucaspalma@gmail.com>
2023-07-04 23:26:15 -0300

    upd interface

--- src/interfaces/IBoardDao.ts
+++ src/interfaces/IBoardDao.ts
@@ -15,4 +15,35 @@ export interface IBoardDao {
 		boardId: string,
 		userId: string
 	): Promise<UsersSharedBoardAssociation | null>;
+	addColumnToBoard(params: ColumnCreationPayload): Promise<void>;
+	addCardToBoard(params: CardCreationPayload): Promise<void>;
+	reorderColumns(params: ReorderColumnParams): Promise<void>;
+	organizeCards(params: OrganizeCardsParams): Promise<void>;
+	updateCardContent(params: UpdateCardContent): Promise<void>;
 }
+
+export type CardCreationPayload = {
+	columnId: string;
+	index: number;
+};
+
+export type ColumnCreationPayload = {
+	boardId: string;
+	title?: string;
+};
+
+export type ReorderColumnParams = {
+	id: number;
+	index: number;
+}[];
+
+export type OrganizeCardsParams = {
+	id: string;
+	columnId: string;
+	index: number;
+}[];
+
+export type UpdateCardContent = {
+	id: string;
+	content: string;
+};
