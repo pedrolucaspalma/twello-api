7b62720455b2fc9c8bdc030fb43b99223204e0b0
Vítor Barroso <contact@owozsh.dev>
2023-07-09 02:40:17 -0300

    upd

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import "reflect-metadata";
 import dotenv from "dotenv";
 import express, { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
+import cors from "cors";
 
 import router from "./routes";
 
@@ -16,26 +17,29 @@ const port = process.env.PORT;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Enabling cors
+app.use(cors());
+
 // Adding app routes
 app.use(router);
 
 // Adding middlewares to be called after every route.
 // 404 error middleware
 app.use((req: Request, res: Response, next: NextFunction) =>
-	next(new StatusError(404, "Route not found"))
+  next(new StatusError(404, "Route not found"))
 );
 // Global Error handler middleware
 app.use(
-	(err: StatusErrorType, req: Request, res: Response, next: NextFunction) => {
-		console.log(`[error]: ${err.message}`);
-		if (err.message && err.status)
-			return res.status(err.status).send({ message: err.message });
+  (err: StatusErrorType, req: Request, res: Response, next: NextFunction) => {
+    console.log(`[error]: ${err.message}`);
+    if (err.message && err.status)
+      return res.status(err.status).send({ message: err.message });
 
-		return res.status(500).send({ message: "Internal server Error" });
-	}
+    return res.status(500).send({ message: "Internal server Error" });
+  }
 );
 
 // Initializing server
 app.listen(port, () => {
-	console.log(`[server]: Server is running at http://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
