ad75dcda4c27d88a84ebd1666abc43f32daa26ec
Pedro Palma <pedrolucaspalma@gmail.com>
2023-07-09 19:57:27 -0300

    add sharedBoards migration

---
+++ src/database/migrations/1688943192102-CreateSharedBoards.ts
@@ -0,0 +1,29 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CreateSharedBoards1688943192102 implements MigrationInterface {
+	public async up(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(
+			`CREATE TABLE "SharedBoards" (
+                "id" UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+                "userId" UUID NOT NULL,
+                "boardId" UUID NOT NULL,
+                "canEdit" VARCHAR(50) NOT NULL,
+                "isFavorite" VARCHAR(50) NOT NULL,
+                "createdAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+				"updatedAt" TIMESTAMP NOT NULL DEFAULT NOW(),
+
+                CONSTRAINT fk_userId_SharedBoards
+                    FOREIGN KEY("userId")
+                        REFERENCES "Users"("id"),
+
+                CONSTRAINT fk_boardId_SharedBoards
+                    FOREIGN KEY("boardId")
+                        REFERENCES "Boards"("id")
+            )`
+		);
+	}
+
+	public async down(queryRunner: QueryRunner): Promise<void> {
+		await queryRunner.query(`DROP TABLE "Boards"`);
+	}
+}
