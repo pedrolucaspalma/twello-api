ea1d70a9bb9addbbe0dbe3be814c4b0971be13d3
Pedro Palma <pedrolucaspalma@gmail.com>
2023-08-09 15:06:54 -0300

    delete all DAOs implementations

--- src/daos/BoardDao.ts
+++ src/daos/BoardDao.ts
@@ -1,4 +1,3 @@
-import { In } from "typeorm";
 import {
 	CardCreationPayload,
 	ColumnCreationPayload,
@@ -16,198 +15,65 @@ import { Board, BoardType } from "../entity/Board";
 import { UserBoard, SharedBoardType } from "../entity/UserBoard";
 import { UserType } from "../entity/User";
 
-const boardsRepository = AppDataSource.getRepository("Board");
-const sharedBoardRepository = AppDataSource.getRepository("UserBoards");
 
 export class BoardDao implements IBoardDao {
-	constructor(private readonly userDao: IUserDao) {}
+	constructor(private readonly userDao: IUserDao) { }
 	async getBoardsSharedWithUser(userId: string): Promise<BoardType[] | []> {
-		const boardRelations = (await sharedBoardRepository.find({
-			where: { userId, isOwner: false },
-		})) as SharedBoardType[];
-		const boardIds = boardRelations.map((r) => r.boardId);
-
-		const boards = (await boardsRepository.find({
-			where: { id: In(boardIds) },
-			order: { createdAt: "DESC" },
-		})) as BoardType[];
-
-		const formattedBoards: Array<
-			BoardType & { isFavorite: boolean; relationId: string }
-		> = [];
-
-		for (const b of boards) {
-			const association = boardRelations.find((r) => r.boardId === b.id);
-			if (!association) continue;
-			formattedBoards.push({
-				...b,
-				isFavorite: association.isFavorite,
-				relationId: association.id,
-			});
-		}
-
-		return formattedBoards;
 	}
 
 	async getBoardsOwnedByUser(userId: string): Promise<BoardType[]> {
-		const boardRelations = (await sharedBoardRepository.find({
-			where: { userId, isOwner: true },
-		})) as SharedBoardType[];
-		const boardIds = boardRelations.map((r) => r.boardId);
-
-		const boards = (await boardsRepository.find({
-			where: { id: In(boardIds) },
-			order: { createdAt: "DESC" },
-		})) as BoardType[];
-
-		const formattedBoards: Array<
-			BoardType & { isFavorite: boolean; relationId: string }
-		> = [];
-
-		for (const b of boards) {
-			const association = boardRelations.find((r) => r.boardId === b.id);
-			if (!association) continue;
-			formattedBoards.push({
-				...b,
-				isFavorite: association.isFavorite,
-				relationId: association.id,
-			});
-		}
-
-		return formattedBoards;
 	}
 
 	async createBoard(data: BoardCreationPayload): Promise<BoardType | null> {
-		const board = new Board();
-
-		board.title = data.title ?? "New Board";
-		board.backgroundColor = data.backgroundColor ?? "#FFFFFF";
-		board.textColor = data.textColor ?? "#000000";
-		await board.save();
-
-		const association = new UserBoard();
-		association.userId = data.ownerUserId;
-		association.boardId = board.id;
-		association.isOwner = true;
-		association.canEdit = true;
-		association.isFavorite = false;
-		await association.save();
-
-		return this.getBoard(board.id);
 	}
 
 	async deleteBoard(boardId: string): Promise<void> {
-		const board = await boardsRepository.findOne({
-			where: { id: boardId },
-		});
-
-		if (!board) return;
-		await sharedBoardRepository.delete({ boardId });
-		await boardsRepository.delete({ id: boardId });
 	}
 
 	async getBoard(boardId: string): Promise<BoardType | null> {
-		const board = await boardsRepository.findOne({
-			where: { id: boardId },
-		});
-
-		return board as BoardType;
 	}
 
 	async getRelationById(relationId: string): Promise<SharedBoardType | null> {
-		const relation = await sharedBoardRepository.findOne({
-			where: { id: relationId },
-		});
-
-		return relation as SharedBoardType;
 	}
 
 	async getUserAssociationWithBoard(
 		boardId: string,
 		userId: string
 	): Promise<SharedBoardType | null> {
-		const association = await sharedBoardRepository.findOne({
-			where: { boardId, userId },
-		});
-		return association as SharedBoardType;
 	}
 
 	async updateBoard(
 		boardId: string,
 		data: BoardUpdatePayload
 	): Promise<BoardType | null> {
-		const board = await boardsRepository.findOne({ where: { id: boardId } });
-
-		if (!board) return null;
-
-		board.title = data.title ?? board.title;
-		board.backgroundColor = data.backgroundColor ?? board.backgroundColor;
-		board.textColor = data.textColor ?? board.textColor;
-		board.content = data.content ?? board.content;
-		await board.save();
-
-		return this.getBoard(board.id);
 	}
 
 	async listUsersRelatedToBoard(
 		boardId: string
 	): Promise<Array<SharedBoardType & { email: string }>> {
-		const associations = (await sharedBoardRepository.find({
-			where: { boardId },
-		})) as SharedBoardType[];
-
-		const returnValues: Array<SharedBoardType & { email: string }> = [];
-
-		for (const a of associations) {
-			const user: UserType = await this.userDao.findById(a.userId);
-			if (user) returnValues.push({ ...a, email: user.email });
-		}
-		return returnValues;
 	}
 
 	async createRelationBetweenUserAndBoard(
 		params: CreateRelationParams
 	): Promise<SharedBoardType | null> {
-		const association = new UserBoard();
-		association.userId = params.userId;
-		association.boardId = params.boardId;
-		association.canEdit = params.canEdit;
-		association.isFavorite = false;
-		await association.save();
-
-		return this.getUserAssociationWithBoard(params.boardId, params.userId);
 	}
 
 	async deleteRelationBetweenUserAndBoard(associationId: string) {
-		const association = await sharedBoardRepository.findOne({
-			where: { id: associationId },
-		});
-		if (!association) return false;
-		await association.remove();
-		return true;
 	}
 
 	async updateRelation(
 		relationId: string,
 		fields: { isFavorite: boolean; canEdit: boolean }
 	): Promise<SharedBoardType | null> {
-		const relation = await sharedBoardRepository.findOne({
-			where: { id: relationId },
-		});
-		if (!relation) return null;
-		relation.isFavorite = fields.isFavorite ?? relation.isFavorite;
-		relation.canEdit = fields.canEdit ?? relation.canEdit;
-		await relation.save();
-		return this.getRelationById(relationId);
 	}
 
-	async addColumnToBoard(params: ColumnCreationPayload): Promise<void> {}
+	async addColumnToBoard(params: ColumnCreationPayload): Promise<void> { }
 
-	async reorderColumns(params: ReorderColumnParams): Promise<void> {}
+	async reorderColumns(params: ReorderColumnParams): Promise<void> { }
 
-	async addCardToBoard(params: CardCreationPayload): Promise<void> {}
+	async addCardToBoard(params: CardCreationPayload): Promise<void> { }
 
-	async organizeCards(params: OrganizeCardsParams): Promise<void> {}
+	async organizeCards(params: OrganizeCardsParams): Promise<void> { }
 
-	async updateCardContent(params: UpdateCardContent): Promise<void> {}
+	async updateCardContent(params: UpdateCardContent): Promise<void> { }
 }
--- src/daos/UserDao.ts
+++ src/daos/UserDao.ts
@@ -1,60 +1,29 @@
 import { IUserDao } from "../interfaces/IUserDao";
 import { CreateUserPayload } from "../types/UserTypes";
 import { User, UserType } from "../entity/User";
-import { AppDataSource } from "../database/data-source";
 import { ChangePasswordRequest } from "../entity/ChangePasswordRequest";
 
-const usersRepository = AppDataSource.getRepository("User");
-
 export class UserDao implements IUserDao {
-	constructor() {}
+	constructor() { }
 
 	async isEmailAvailable(email: string): Promise<boolean> {
-		const user = await usersRepository.findOne({ where: { email } });
-		if (user) return false;
-		return true;
 	}
 
 	async create(userCreationPayload: CreateUserPayload): Promise<UserType> {
-		const user = new User();
-		user.name = userCreationPayload.name;
-		user.email = userCreationPayload.email;
-		user.password = userCreationPayload.password;
-		await user.save();
-		return this.findById(user.id);
 	}
 
 	async setPasswordToken(userId: string, passwordToken: string): Promise<void> {
-		const request = new ChangePasswordRequest();
-		request.userId = userId;
-		request.passwordToken = passwordToken;
-		await request.save();
 	}
 
 	async updatePassword(userId: string, newPassword: string): Promise<UserType> {
-		const user = await usersRepository.findOne({ where: { id: userId } });
-		if (!user) throw new Error("User not found");
-		user.password = newPassword;
-		await user.save();
-		return this.findById(user.id);
 	}
 
 	async findById(id: string): Promise<UserType> {
-		const userLiteral: any = await usersRepository.findOne({
-			where: { id },
-		});
-
-		return userLiteral as UserType;
 	}
 
 	async findByEmail(email: string): Promise<UserType | null> {
-		const user = await usersRepository.findOne({ where: { email } });
-		if (!user) return null;
-		return user as UserType;
 	}
 
 	async listUsers(): Promise<UserType[]> {
-		const users = await usersRepository.findBy({});
-		return users as UserType[];
 	}
 }
