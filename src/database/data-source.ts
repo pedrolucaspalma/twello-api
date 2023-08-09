86aa03bfdb98a940e650d3c844013f222226ff03
Pedro Palma <pedrolucaspalma@gmail.com>
2023-08-09 15:11:58 -0300

    delete TypeORM code from rest of codebase

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,5 +1,4 @@
 import "reflect-metadata";
-import { DataSource } from "typeorm";
 import { User } from "../entity/User";
 import { Board } from "../entity/Board";
 import { UserBoard } from "../entity/UserBoard";
@@ -15,16 +14,4 @@ const {
 
 const port = Number(DATABASE_INTERNAL_PORT);
 
-export const AppDataSource = new DataSource({
-	type: "postgres",
-	host: DATABASE_HOST,
-	port,
-	username: DATABASE_USERNAME,
-	password: DATABASE_PASSWORD,
-	database: DATABASE_SCHEMA,
-	synchronize: false,
-	logging: false,
-	entities: [User, Board, UserBoard, ChangePasswordRequest],
-	migrations: ["build/database/migrations/**/*.js"],
-	subscribers: [],
-});
+export const AppDataSource = {}
