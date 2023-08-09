0169f73fe5347b7cf8a5a8918a3c472f3ab06404
Pedro Palma <pedrolucaspalma@gmail.com>
2023-08-09 16:47:17 -0300

    make dao methods throw errors

--- src/daos/BoardDao.ts
+++ src/daos/BoardDao.ts
@@ -18,52 +18,64 @@ import { UserType } from "../models/User";
 export class BoardDao implements IBoardDao {
 	constructor(private readonly userDao: IUserDao) { }
 	async getBoardsSharedWithUser(userId: string): Promise<BoardType[] | []> {
+		throw new Error("not implemented")
 	}
 
 	async getBoardsOwnedByUser(userId: string): Promise<BoardType[]> {
+		throw new Error("not implemented")
 	}
 
 	async createBoard(data: BoardCreationPayload): Promise<BoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async deleteBoard(boardId: string): Promise<void> {
+		throw new Error("not implemented")
 	}
 
 	async getBoard(boardId: string): Promise<BoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async getRelationById(relationId: string): Promise<SharedBoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async getUserAssociationWithBoard(
 		boardId: string,
 		userId: string
 	): Promise<SharedBoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async updateBoard(
 		boardId: string,
 		data: BoardUpdatePayload
 	): Promise<BoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async listUsersRelatedToBoard(
 		boardId: string
 	): Promise<Array<SharedBoardType & { email: string }>> {
+		throw new Error("not implemented")
 	}
 
 	async createRelationBetweenUserAndBoard(
 		params: CreateRelationParams
 	): Promise<SharedBoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async deleteRelationBetweenUserAndBoard(associationId: string) {
+		throw new Error("not implemented")
 	}
 
 	async updateRelation(
 		relationId: string,
 		fields: { isFavorite: boolean; canEdit: boolean }
 	): Promise<SharedBoardType | null> {
+		throw new Error("not implemented")
 	}
 
 	async addColumnToBoard(params: ColumnCreationPayload): Promise<void> { }
--- src/daos/UserDao.ts
+++ src/daos/UserDao.ts
@@ -7,23 +7,30 @@ export class UserDao implements IUserDao {
 	constructor() { }
 
 	async isEmailAvailable(email: string): Promise<boolean> {
+		throw new Error("not implemented")
 	}
 
 	async create(userCreationPayload: CreateUserPayload): Promise<UserType> {
+		throw new Error("not implemented")
 	}
 
 	async setPasswordToken(userId: string, passwordToken: string): Promise<void> {
+		throw new Error("not implemented")
 	}
 
 	async updatePassword(userId: string, newPassword: string): Promise<UserType> {
+		throw new Error("not implemented")
 	}
 
 	async findById(id: string): Promise<UserType> {
+		throw new Error("not implemented")
 	}
 
 	async findByEmail(email: string): Promise<UserType | null> {
+		throw new Error("not implemented")
 	}
 
 	async listUsers(): Promise<UserType[]> {
+		throw new Error("not implemented")
 	}
 }
